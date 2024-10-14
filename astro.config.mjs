703ed70fded51890a60d17e22b7e79ce341496c3
frederic leveque <fred-l@hotmail.com>
2024-10-14 20:55:00 +0700

    Revert "add node adpater"
    This reverts commit c556c92c65efc637031b35cb1dc39b1d5102b421.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://seavue.xyz',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
