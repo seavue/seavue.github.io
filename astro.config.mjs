c556c92c65efc637031b35cb1dc39b1d5102b421
frederic leveque <fred-l@hotmail.com>
2024-10-14 20:43:17 +0700

    add node adpater

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://seavue.xyz',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
